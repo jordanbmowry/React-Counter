e0d0e6f8358916eae2735aa10dfd3ecaaab8398b
Jordan Mowry <jordan.mowry@gmail.com>
2021-06-11 08:44:37 -0700

    rendered three additional Buttons in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Button from './components/Button';
 function App() {
 	return (
 		<div className="App">
-			<Button />
+			<Button increment={1} />
+			<Button increment={10} />
+			<Button increment={100} />
+			<Button increment={1000} />
 		</div>
 	);
 }
